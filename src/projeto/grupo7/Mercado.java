5ee98fbd1920ec0df5734d9c2c300ff401b31523
Gabriel <ghsoares99795@gmail.com>
2022-03-13 17:46:59 -0300

    Implemented a bit of the Mercado class

--- src/projeto/grupo7/Mercado.java
+++ src/projeto/grupo7/Mercado.java
@@ -1,60 +1,64 @@
 package projeto.grupo7;
 
-import java.util.ArrayList;
-import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class Mercado {
+	// Variáveis do estoque e do carrinho
+	private static Estoque estoque;
+	private static Carrinho carrinho;
+
+	// Método para mostrar e pegar a opção do menu
+	public static int menu(Scanner leia) {
+		// Mostra o menu
+		System.out.println("\nInsira uma opção:");
+
+		System.out.println("\n1 - Adicionar um produto ao carrinho.");
+		System.out.println("2 - Checar o carrinho.");
+		System.out.println("3 - Sair do programa.");
+
+		// Ler a opção
+		int op = leia.nextInt();
+
+		// Continua lendo a opção caso for inválida
+		while (op < 1 || op > 3) {
+			System.out.println("\nOpção inválida! Tenta de novo:");
+			op = leia.nextInt();
+		}
+
+		// Retorna a opção
+		return op;
+	}
+	
+	// Método para adicionar um produto ao carrinho
+	public static void adicionarAoCarrinho() {
+		
+	}
 
 	public static void main(String[] args) {
-		// Criando array list do estoque do mercado
-		ArrayList<Produto> estoque  = new ArrayList<Produto>();
-		
-		// Criando array list do carrinho de compras
-		ArrayList<Produto> carrinho = new ArrayList<Produto>();
+		// Criação do estoque e do carrinho
+		estoque = new Estoque();
+		carrinho = new Carrinho();
 		
 		// Criação de um objeto scanner para ler a entrada do usuário
 		Scanner leia = new Scanner(System.in);
-		
+
 		System.out.println("\nSeja bem vindo ao mercado!");
-		
-		Inventario inv = new Estoque();
-		
-		inv.adicionarProduto(32, new ProdutoPerecivel());
-		inv.adicionarProduto(16, new ProdutoPerecivel());
-		inv.adicionarProduto(47, new ProdutoPerecivel());
-		
-		for (int i = 0; i < inv.getQuantidadeElementos(); i++) {
-			int cod = inv.getCodigo(i);
-			System.out.println(cod);
-			System.out.println(inv.getProduto(cod).getNome());
-		}
-		
-		/*// Opção do menu
+
 		int op;
-		
-		for (Entry<Integer, Produto> chaveValor : estoque.getEntries()) {
-			
-		}
-		
+
 		do {
-			System.out.println("\n");
-			
-			if (estoque.tentarSubtrairQuantidade(3, 5)) {
-				carrinho.adicionarProduto(estoque.getProduto(3), 5);
-			} else {
-				System.out.println("\nEstoque não tem quantidade suficiente desse produto.");
+			// Mostrar o menu e pegar a opção
+			op = menu(leia);
+
+			switch (op) {
+				case 1: { // Adicionar um produto ao carrinho
+					break;
+				}
 			}
+
 		} while (op != 3);
-		
-		System.out.println("\nAté mais!");*/
+
+		System.out.println("\nAté mais!");
 	}
 
 }
-
-
-
-
-
-
-
