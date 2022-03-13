197c87e26b8cd00b92352230a6d3f21bee44422a
Gabriel <ghsoares99795@gmail.com>
2022-03-13 17:25:36 -0300

    Changed interface Produto to class

--- src/projeto/grupo7/Produto.java
+++ src/projeto/grupo7/Produto.java
@@ -1,10 +1,46 @@
 package projeto.grupo7;
 
-public interface Produto {
-	String 	getNome();
-	int 	getQuantidade();
-	float 	getPreco();
-	float 	getPorcentagemDoacao();
+public class Produto {
+	private String nome;
+	private int quantidade;
+	private float preco;
+	private float porcentagemDoacao;
 	
-	void	adicionarQuantidade(int qtd);
+	// Método que adiciona quantidade do produto
+	public void adicionarQuantidade(int qtd) {
+		this.quantidade += qtd;
+	}
+	
+	// Getters e setters
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public float getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	
+	public float getPorcentagemDoacao() {
+		return porcentagemDoacao;
+	}
+	
+	public void setPorcentagemDoacao(float porcentagemDoacao) {
+		this.porcentagemDoacao = porcentagemDoacao;
+	}
 }
